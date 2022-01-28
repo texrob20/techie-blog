d34a95f8474a3a2388e3d0e83ee18a5968bcaed2
Robert C <robert.clarady@gmail.com>
2022-01-28 15:41:40 -0600

    added logout function

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -135,7 +135,8 @@ router.delete('/:id', withAuth, (req, res) => {
       });
   });
 
-router.post('/logout', withAuth, (req, res) => {
+router.post('/logout', (req, res) => {
+  console.log(req.session.loggedIn);
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
