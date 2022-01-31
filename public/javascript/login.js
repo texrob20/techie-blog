3ccb6a264c58a4996bdc026bf4beb38d93e37c04
Robert C <robert.clarady@gmail.com>
2022-01-31 13:32:09 -0600

    split login and signup functions, updated CSS

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,26 +1,4 @@
-async function signupFormHandler(event) {
-    event.preventDefault();
-    const username = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && password) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      // check the response status
-      if (response.ok) {
-        console.log('success');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  }
+
     
   async function loginFormHandler(event) {
     event.preventDefault();
@@ -46,5 +24,4 @@ async function signupFormHandler(event) {
     }
   }
   
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+    document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
